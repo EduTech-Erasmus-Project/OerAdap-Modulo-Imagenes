fbefca70ea84ba4328b8d819c4ffdd5aa69e5f6c
EduTech <calvaradot1@est.ups.edu.ec>
2021-10-05 08:45:41 -0500

    utility.py now try to read an image, if it works saves the path and its belonging class to dataframe.

---
+++ Level2 Illustration/complex_classification/vision-transformers/utlity.py
@@ -0,0 +1,43 @@
+import glob
+import imghdr
+import os
+import pandas
+
+root_path = '/home/edutech-pc06/Documentos/DATASETS/LVL1/Illustration'
+image_path_directory = dict()
+
+
+def read_charts():
+    for root, dirs, files in os.walk(root_path+'/Chart'):
+        for file in files:
+            if imghdr.what(os.path.join(root, file)) is not None:
+                image_path_directory[os.path.join(root, file)] = 0
+            else:
+                print(f'Error while reading file: {file}')
+
+
+def read_digital():
+    for root, dirs, files in os.walk(root_path+'/Digital'):
+        for file in files:
+            if imghdr.what(os.path.join(root, file)) is not None:
+                image_path_directory[os.path.join(root, file)] = 1
+            else:
+                print(f'Error while reading file: {file}')
+
+
+def read_photo():
+    for root, dirs, files in os.walk(root_path+'/Photo'):
+        for file in files:
+            if imghdr.what(os.path.join(root, file)) is not None:
+                image_path_directory[os.path.join(root, file)] = 2
+            else:
+                print(f'Error while reading file: {file}')
+
+
+read_charts()
+read_digital()
+read_photo()
+
+df = pandas.DataFrame.from_dict(image_path_directory.items())
+df.columns = ['path', 'class']
+df.to_csv('dataframe.csv', index=False)
